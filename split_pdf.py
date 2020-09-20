d72bb7f0d9cb7dbdb9bbc6069117a1d948b69025
Subroto Pinku <subrotonpi@users.noreply.github.com>
2020-09-21 04:14:59 +0600

    split_pdf

---
+++ split_pdf.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[10]:
+
+
+# pdf_splitter.py
+
+import os
+from PyPDF2 import PdfFileReader, PdfFileWriter
+import pandas as pd
+df = pd.read_excel('letters/info.xlsx', index_col=0)  
+
+def pdf_splitter(path):
+    fname = os.path.splitext(os.path.basename(path))[0]
+
+    pdf = PdfFileReader(path)
+    for page in range(pdf.getNumPages()):
+        pdf_writer = PdfFileWriter()
+        pdf_writer.addPage(pdf.getPage(page))
+        
+        output_filename = 'ID__{}___Guardian___{}___.pdf'.format(
+            df['ID (in English)'].values[page], 
+            df['বাবা/মা/অভিভাবক এর নাম(বাংলায়)'].values[page]
+        )
+         
+        with open(output_filename, 'wb') as out:
+            pdf_writer.write(out)
+
+        print('Created: {}'.format(output_filename))
+
+if __name__ == '__main__':
+    path = 'letters/merged.pdf'
+    pdf_splitter(path)
+
